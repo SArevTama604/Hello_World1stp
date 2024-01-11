4aede57fa32a301998cf74995e45e32c85a03524
Salvador <sarevalotamayo@student.rtc.edu>
2024-01-10 18:53:14 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hello_World1stp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Salvador Arevalo!");
+        }
+    }
+}
